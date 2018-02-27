8876f7cbebfdc70ba470abf613c3dde480e46012
Andrea Borgotti <andrea.borgotti@gmail.com>
2018-02-27 14:18:32 +0100

    Set button function

--- Tris/ViewController.swift
+++ Tris/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var b: UIButton!
+    var player = 1
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    
+    @IBAction func clickOnButton(_ sender: AnyObject)
+    {
+        if(gameState[sender.tag-1] == 0)
+        {
+            if(player == 1){
+                sender.setImage(UIImage(named: "cervello.png"), for: UIControlState())
+                player = 2
+                gameState[sender.tag-1] = 1
+            }else{
+                sender.setImage(UIImage(named: "cuore.png"), for: UIControlState())
+                player = 1
+                gameState[sender.tag-1] = 2
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
