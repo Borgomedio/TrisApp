40cb84612daad796dd192508faadf1f7686fd99e
Andrea Borgotti <andrea.borgotti@gmail.com>
2018-02-27 15:31:49 +0100

    game is ok, but i have to check same game case

--- Tris/ViewController.swift
+++ Tris/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var b: UIButton!
     var player = 1
     var gameState = [0,0,0,0,0,0,0,0,0]
     
     let winningCombination = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     
     var gameIsActive = true
+    
+    
+    
+    @IBOutlet weak var labelWinner: UILabel!
+    @IBOutlet weak var buttonReplay: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
     
     @IBAction func clickOnButton(_ sender: AnyObject)
     {
@@ -35,22 +40,45 @@ class ViewController: UIViewController {
         
         for combination in winningCombination
         {
-            if(gameState[combination[0]] != 0 && gameState[combination[0]]==gameState[combination[1]] && gameState[combination[1]]==gameState[combination[2]]){
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
                 gameIsActive = false
-                if (gameState[combination[0]]==1)
+                if gameState[combination[0]] == 1
                 {
                     //cervello win
                     print("CERVELLO win!")
+                    labelWinner.text = "Vince CERVELLO!"
                 }else
                 {
                     //cuore win
                     print("CUORE win!")
+                    labelWinner.text = "Vince CUORE!"
                 }
+                
+                labelWinner.isHidden = false
+                buttonReplay.isHidden = false
             }
+        }
+        
+        
+    }
+    
+   
+    @IBAction func clickOnButtonReplay(_ sender: AnyObject) {
+        gameIsActive = true
+        labelWinner.isHidden = true
+        buttonReplay.isHidden = true
+        gameState = [0,0,0,0,0,0,0,0,0]
+        player = 1
+        
+        for i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControlState())
         }
     }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
