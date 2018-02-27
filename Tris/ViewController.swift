ced2652969897fc580a6527e5bf6a6e9e8d4e175
Andrea Borgotti <andrea.borgotti@gmail.com>
2018-02-27 11:30:26 +0100

    first commit

---
+++ Tris/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tris
+//
+//  Created by Andrea on 27/02/18.
+//  Copyright © 2018 Andrea Borgotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
